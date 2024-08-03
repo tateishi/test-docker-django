676b38c7321703e87b1cb6bf5946729c7b5d1542
Tateishi Tadatoshi <ishio39@gmail.com>
2024-08-03 15:39:37 +0900

    githubからアプリケーションを取得するように変更

--- django/settings_ex.py
+++
@@ -1,8 +0,0 @@
-ALLOWED_HOSTS = ["*"]
-LANGUAGE_CODE = "ja-jp"
-TIME_ZONE = "Asia/Tokyo"
-STATIC_ROOT = BASE_DIR / "static"
-
-n = MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")
-MIDDLEWARE.insert(n+1, "whitenoise.middleware.WhiteNoiseMiddleware")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
